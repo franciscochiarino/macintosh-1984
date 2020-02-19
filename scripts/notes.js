1c48b01d69ec0c94ba96ac5e71a4998eb4a9c1b1
fran <francisco.chiarino@gmail.com>
2020-02-19 15:12:11 +0100

    Get server data everytime we open notes

---
+++ scripts/notes.js
@@ -0,0 +1,65 @@
+function notes() {
+
+    // Variables
+    const form = document.querySelector('form');
+    let ul = document.querySelector('ul');
+    let tasks = [];
+
+    serverData = sessionStorage.getItem('item');
+    
+    if (serverData) {
+        tasks = serverData;
+    }
+    console.log(tasks)
+    
+
+    // Add Note
+    const addNote = (e) => {
+        e.preventDefault();
+        ul.innerHTML = '';
+
+        // Push string to taks array
+        input = document.querySelector('input[type="text"]');
+        tasks.push(input.value);
+        sessionStorage.setItem('item', tasks);
+        input.value = '';
+
+        // Create elements for each task
+        tasks.forEach(task => {
+
+            // List
+            const li = document.createElement('li');
+
+            // Span 
+            const spanX = document.createElement('span');
+            const x = document.createTextNode('x');
+            spanX.appendChild(x);
+
+            // Text
+            const textP = document.createElement('p');
+            const text = document.createTextNode(task);
+            textP.appendChild(text);
+            
+            // Append elements
+            li.append(textP, spanX);
+            ul.appendChild(li);
+        })
+
+    }
+
+    // Remove Note
+    const removeNote = event => {
+        let clickedText = event.target.parentElement.firstChild.innerHTML;
+
+        if (event.target.tagName === 'SPAN') {
+            let index = tasks.indexOf(clickedText);
+            tasks.splice(index, 1);
+            event.target.parentElement.remove()
+            
+        }
+    }
+
+    form.addEventListener('submit', addNote);
+    ul.addEventListener('click', removeNote);
+
+}
