0e409870b66b9e862d85d96a7aab442f140fa73e
fran <francisco.chiarino@gmail.com>
2020-03-07 18:42:07 +0100

    Put the calculator script inside calculator()

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,98 +1,102 @@
-const buttons = document.querySelectorAll('button');
-const display = document.getElementById('display');
-
-// Calculations
-const sum = (a, b) => a + b;
-const take = (a, b) => a - b;
-const divide = (a, b) => a / b;
-const multiply = (a, b) => a * b;
-const clear = () => display.innerHTML = '';
-
- //Check if there already is an operator and run the corresponding function
- const runCalculation = () => {
-    let operator = event.target.innerHTML;
+function calculator() {
     
-    if (display.innerHTML.includes('+')) {
-        let splitedDisplay = display.innerHTML.split('+');
-        let numbers = splitedDisplay.map(n => parseFloat(n));
-        let result = sum(numbers[0], numbers[1]);
-        displayResult(result, operator);
+    const buttons = document.querySelectorAll('button');
+    const display = document.getElementById('display');
+
+    // Calculations
+    const sum = (a, b) => a + b;
+    const take = (a, b) => a - b;
+    const divide = (a, b) => a / b;
+    const multiply = (a, b) => a * b;
+    const clear = () => display.innerHTML = '';
+
+    //Check if there already is an operator and run the corresponding function
+    const runCalculation = () => {
+        let operator = event.target.innerHTML;
+        
+        if (display.innerHTML.includes('+')) {
+            let splitedDisplay = display.innerHTML.split('+');
+            let numbers = splitedDisplay.map(n => parseFloat(n));
+            let result = sum(numbers[0], numbers[1]);
+            displayResult(result, operator);
+        }
+
+        else if (display.innerHTML.includes('-')) {
+            let splitedDisplay = display.innerHTML.split('-');
+            let numbers = splitedDisplay.map(n => parseFloat(n));
+            let result = take(numbers[0], numbers[1]);
+            displayResult(result, operator);
+        }
+
+        else if (display.innerHTML.includes('/')) {
+            let splitedDisplay = display.innerHTML.split('/');
+            let numbers = splitedDisplay.map(n => parseFloat(n));
+            let result = divide(numbers[0], numbers[1]);
+            displayResult(result, operator);
+        }
+
+        else if (display.innerHTML.includes('*')) {
+            let splitedDisplay = display.innerHTML.split('*');
+            let numbers = splitedDisplay.map(n => parseFloat(n));
+            let result = multiply(numbers[0], numbers[1]);
+            displayResult(result, operator);
+        }
+        
+        else {
+            display.innerHTML += operator;
+        }
     }
 
-    else if (display.innerHTML.includes('-')) {
-        let splitedDisplay = display.innerHTML.split('-');
-        let numbers = splitedDisplay.map(n => parseFloat(n));
-        let result = take(numbers[0], numbers[1]);
-        displayResult(result, operator);
-    }
+    // Display 
+    const displayContent = event => {
 
-    else if (display.innerHTML.includes('/')) {
-        let splitedDisplay = display.innerHTML.split('/');
-        let numbers = splitedDisplay.map(n => parseFloat(n));
-        let result = divide(numbers[0], numbers[1]);
-        displayResult(result, operator);
-    }
+        // Get last character of display
+        let displayLastCharacter = parseInt(display.innerHTML[display.innerHTML.length - 1], 10);
 
-    else if (display.innerHTML.includes('*')) {
-        let splitedDisplay = display.innerHTML.split('*');
-        let numbers = splitedDisplay.map(n => parseFloat(n));
-        let result = multiply(numbers[0], numbers[1]);
-        displayResult(result, operator);
-    }
-    
-    else {
-        display.innerHTML += operator;
-    }
-}
+        // Numbers
+        if (event.target.className === 'number') {
+            display.innerHTML += event.target.innerHTML;
+        }
 
-// Display 
-const displayContent = event => {
+        // Clear
+        if (event.target.innerHTML === 'E') {
+            display.innerHTML += '2.71';
+        }
 
-    // Get last character of display
-    let displayLastCharacter = parseInt(display.innerHTML[display.innerHTML.length - 1], 10);
+        // Clear
+        if (event.target.innerHTML === 'C') {
+            display.innerHTML = '';
+        }
 
-    // Numbers
-    if (event.target.className === 'number') {
-        display.innerHTML += event.target.innerHTML;
-    }
+        // Operators
+        if (event.target.className === 'operator') {
 
-    // Clear
-    if (event.target.innerHTML === 'E') {
-        display.innerHTML += '2.71';
-    }
+            // Check if last character is a number *
+            if (displayLastCharacter >= 0 && displayLastCharacter <= 9) {
+                runCalculation();
+            }   
+        }
 
-    // Clear
-    if (event.target.innerHTML === 'C') {
-        display.innerHTML = '';
-    }
-
-    // Operators
-    if (event.target.className === 'operator') {
-
-        // Check if last character is a number *
-        if (displayLastCharacter >= 0 && displayLastCharacter <= 9) {
+        // Equal
+        if (event.target.innerHTML === '=') {
             runCalculation();
-        }   
+            display.innerHTML = display.innerHTML.substring(0, display.innerHTML.length - 1);
+        } 
     }
 
-    // Equal
-    if (event.target.innerHTML === '=') {
-        runCalculation();
-        display.innerHTML = display.innerHTML.substring(0, display.innerHTML.length - 1);
-    } 
-}
+    // Display Result
+    const displayResult = (result, operator) => {
 
-// Display Result
-const displayResult = (result, operator) => {
-
-    if (result.toString().includes('.')) {
-        display.innerHTML = result.toFixed(2) + operator;
-    } else {
-        display.innerHTML = result + operator;
+        if (result.toString().includes('.')) {
+            display.innerHTML = result.toFixed(2) + operator;
+        } else {
+            display.innerHTML = result + operator;
+        }
     }
-}
 
-// Event listeners
-buttons.forEach(button => button.addEventListener('click', displayContent))
+    // Event listeners
+    buttons.forEach(button => button.addEventListener('click', displayContent))
+
+}
 
 // * You might ask yourself why I didn't use typeof to check if it's a number. That's because if you parseInt an operator, JS will tell you it is a number, and I need to know if there is an operator on a number at the end. 
